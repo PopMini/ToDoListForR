a3e8f662ca050276b48e859aa122de06643bf211
hardy <hardy.lu@rocketmail.com>
2018-05-22 19:29:18 +0800

    First android project.

--- app/src/main/java/com/todolist4r/hardy/todolistforr/MainActivity.java
+++ app/src/main/java/com/todolist4r/hardy/todolistforr/MainActivity.java
@@ -1,7 +1,9 @@
 package com.todolist4r.hardy.todolistforr;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+/*
+        TextView tv_hello;
+        tv_hello = (TextView)findViewById(R.id.tv_hello);
+        tv_hello.setText("Today is very hot");
+        tv_hello.setTextColor(Color.RED);
+        tv_hello.setTextSize(30);
+        */
+
     }
 }
